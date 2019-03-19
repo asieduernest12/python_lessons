054a8389dc2923bc2642a007e23e17989428beaa
asieduernest <asieduernest@yahoo.co.uk>
2019-03-19 16:21:06 -0400

    readme inclusion

--- code.py
+++ code.py
@@ -3,23 +3,25 @@
 # program2 outputs a+b
 # program3 outputs even numbers up to x, where x >=2
 
+# greet function outputs a greeting with the name submitted as its input
 def greet(name):
     print("Good evening " + name)
-
+# adder function outputs sum of two inputs 
 def adder(a,b):
     result = str(a + b)
     print("result is => " + result )
 
+# area function prints the multiplicated results of two parameters
 def areaofrec(length,breadth):
     print(str(length * breadth))
 
 
 # main function is the default function called when a python file is executed
 def main():
-    greet("Mensa")
-    greet("Dan")
-    adder(19,1)
-    areaofrec(20,3)
+    greet("Mensa") # calling greet function with "Mensa" the value for name var
+    greet("Dan") # Dan will be value for name var
+    adder(19,1) # calling adder with two inputs to added and printed
+    areaofrec(20,3) 
     areaofrec(22,5)
 
 if __name__ == "__main__":
