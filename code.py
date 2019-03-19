b93b125b31ff9fbdb9ee71c6e72ca82f64cada85
asieduernest <asieduernest@yahoo.co.uk>
2019-03-19 15:38:05 -0400

    how create more functions

--- code.py
+++ code.py
@@ -3,4 +3,24 @@
 # program2 outputs a+b
 # program3 outputs even numbers up to x, where x >=2
 
-def 
+def greet(name):
+    print("Good evening " + name)
+
+def adder(a,b):
+    result = str(a + b)
+    print("result is => " + result )
+
+def areaofrec(length,breadth):
+    print(str(length * breadth))
+
+
+# main function is the default function called when a python file is executed
+def main():
+    greet("Mensa")
+    greet("Dan")
+    adder(19,1)
+    areaofrec(20,3)
+    areaofrec(22,5)
+
+if __name__ == "__main__":
+    main()
